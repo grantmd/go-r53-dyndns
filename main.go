2e19b81dbc4c47822b859050ca17de5e4f61ee1e
Myles Grant <myles@mylesgrant.com>
2019-09-25 09:38:43 -0700

    Stub out some flags

--- main.go
+++ main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 var (
+	awsAccessKeyID     string
+	awsSecretAccessKey string
+
+	domain       string
+	hostedZoneID string
+
 	ipv4 string
 	ipv6 string
 )
 
 func main() {
+	// Parse command-line options
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "usage: ./go-r53-dyndns\n")
+		flag.PrintDefaults()
+	}
+
+	flag.StringVar(&awsAccessKeyID, "awsAccessKeyID", "", "AWS access key ID")
+	flag.StringVar(&awsSecretAccessKey, "awsSecretAccessKey", "", "AWS secret access key")
+	flag.StringVar(&domain, "domain", "", "(sub)domain to manage")
+	flag.StringVar(&hostedZoneID, "hostedZoneID", "", "Hosted zone ID that contains the domain")
+
+	flag.Parse()
+
+	if awsAccessKeyID == "" || awsSecretAccessKey == "" || domain == "" || hostedZoneID == "" {
+		flag.Usage()
+		os.Exit(2)
+	}
+
 	log.Println("Starting up")
 
 	// Get ipv4 address

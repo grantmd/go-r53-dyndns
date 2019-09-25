c59b5bb8caf495ff311981cbf7e24e9b520e5215
Myles Grant <myles@mylesgrant.com>
2019-09-25 08:57:17 -0700

    Switch to a service that will give both ipv6 and ipv4

--- main.go
+++ main.go
@@ -7,14 +7,15 @@ import (
 )
 
 var (
-	ipv4 = ""
-	ipv6 = ""
+	ipv4 string
+	ipv6 string
 )
 
 func main() {
 	log.Println("Starting up")
 
-	resp, err := http.Get("http://ifconfig.co/")
+	// Get ipv4 address
+	resp, err := http.Get("http://ipv4.icanhazip.com/")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -24,7 +25,20 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	log.Printf("IP Address is: %s", body)
+	log.Printf("IPV4 IP Address is: %s", body)
 	ipv4 = string(body)
+
+	// Get ipv6 address
+	resp, err = http.Get("http://ipv6.icanhazip.com/")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("IPV6 IP Address is: %s", body)
 	ipv6 = string(body)
 }

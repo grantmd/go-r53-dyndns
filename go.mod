f292885ba8d8c45d20396662982e1154f2010341
Myles Grant <myles@mylesgrant.com>
2019-09-26 23:13:52 -0700

    Start on the route53 integration

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/grantmd/go-r53-dyndns
 
 go 1.13
+
+require github.com/aws/aws-sdk-go v1.25.0
--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/route53"
 )
 
 var (
@@ -29,8 +34,8 @@ func main() {
 
 	flag.StringVar(&awsAccessKeyID, "awsAccessKeyID", "", "AWS access key ID")
 	flag.StringVar(&awsSecretAccessKey, "awsSecretAccessKey", "", "AWS secret access key")
-	flag.StringVar(&domain, "domain", "", "(sub)domain to manage")
 	flag.StringVar(&hostedZoneID, "hostedZoneID", "", "Hosted zone ID that contains the domain")
+	flag.StringVar(&domain, "domain", "", "(sub)domain to manage")
 
 	flag.Parse()
 
@@ -40,6 +45,18 @@ func main() {
 	}
 
 	log.Println("Starting up")
+
+	// Setup the route53 stuff and figure out what we have
+	sess, err := session.NewSession(&aws.Config{
+		CredentialsChainVerboseErrors: aws.Bool(true),
+		Credentials:                   credentials.NewStaticCredentials(awsAccessKeyID, awsSecretAccessKey, ""),
+	})
+	if err != nil {
+		log.Fatalln("Failed to create session:", err)
+	}
+
+	svc := route53.New(sess)
+	listCNAMES(svc)
 
 	// Get ipv4 address
 	resp, err := http.Get("http://ipv4.icanhazip.com/")
@@ -70,4 +87,29 @@ func main() {
 		log.Println("Neither ipv4 nor ipv6 addresses found. Cowardly giving up.")
 		os.Exit(2)
 	}
+}
+
+func listCNAMES(svc *route53.Route53) {
+	// Now lets list all of the records.
+	// For the life of me, I can't figure out how to get these lists to actually constrain the results.
+	// AFAICT, supplying only the HostedZoneId returns exactly the same results as any valid input in all params.
+	listParams := &route53.ListResourceRecordSetsInput{
+		HostedZoneId: aws.String(hostedZoneID), // Required
+		// MaxItems:              aws.String("100"),
+		// StartRecordIdentifier: aws.String("Sample update."),
+		// StartRecordName:       aws.String("com."),
+		// StartRecordType:       aws.String("CNAME"),
+	}
+	respList, err := svc.ListResourceRecordSets(listParams)
+
+	if err != nil {
+		// Print the error, cast err to awserr.Error to get the Code and
+		// Message from an error.
+		log.Println(err.Error())
+		return
+	}
+
+	// Pretty-print the response data.
+	log.Println("All records:")
+	log.Println(respList)
 }
